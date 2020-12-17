b2705efd3ec6261e73f98c43ad304ead73d3a641
RobBob-beep-bop <59290587+RobBob-beep-bop@users.noreply.github.com>
2020-12-17 15:49:11 -0600

    Update Make Lifetable.R

--- Make Lifetable.R
+++ Make Lifetable.R
@@ -3,10 +3,11 @@
 # this function works with data generated in class with slight modifications to the column names
 # requires a dataframe with column titles LatY, 
 # LongX, Leaves, flower, seeds
-# example: Make_life_table<- (mydata)
+# example: x<- Create_life_table(mydata)
 
-
-
+Create_life_table<- function(x){
+  
+  
 Make_life_table<- function(x) {
   
   #aggregate data by counting summarizing unique rows of the same combinations of coordinates 
@@ -18,65 +19,75 @@ Make_life_table<- function(x) {
     dplyr::rename("Number of leaves (age) x" = Leaves) %>% 
     dplyr::select(-c(sum_flower, sum_seed))
   
-x<- as.list(x)
-#Survivorship 
-
-Sx <- sum(x$Ax) - cumsum(x$Ax) + x$Ax
-lx<- c()
-lx[1]<- 1
-
-for(i in 2:length(x$`Number of leaves (age) x`)){
-  lx[i] <- Sx[i]/Sx[1]
+  x<- as.list(x)
+  #Survivorship 
+  
+  Sx <- sum(x$Ax) - cumsum(x$Ax) + x$Ax
+  lx<- c()
+  lx[1]<- 1
+  
+  for(i in 2:length(x$`Number of leaves (age) x`)){
+    lx[i] <- Sx[i]/Sx[1]
+  }
+  
+  
+  #Reproductive value
+  gx<- c()
+  `lx_bx`<- c()
+  `lx_bx_x`<- c()
+  
+  for (i in 1:length(x$Ax)) {
+    gx[i] <- lx[i+1]/lx[i]
+    
+    `lx_bx`[i]<- lx[i]*x$`bx (seeds/stalk)`[i]
+    
+    `lx_bx_x`[i]<- lx[i]*x$`bx (seeds/stalk)`[i]*x$`Number of leaves (age) x`[i]}
+  
+  Ro<- sum(`lx_bx`)
+  G<- sum(`lx_bx_x`/Ro)
+  R<- (log(Ro)/G)
+  
+  #Stable age distribution
+  `lx*e^rx`<- c()
+  cx<- c()
+  `e^(rx)/lx`<- c()
+  `e^-rx*lx*bx`<- c()
+  `sumofe^-rx*lybx`<- c()
+  vx<- c()
+  
+  
+  for (i in 1:length(x$Ax)) {
+    `lx*e^rx`[i]<- (lx[i])*exp(-R*x$`Number of leaves (age) x`[i])
+    
+    cx[i] <- `lx*e^rx`[i]/sum(`lx*e^rx`)
+    
+    `e^(rx)/lx`[i] <- exp(-R*x$`Number of leaves (age) x`[i])/lx[i]
+    
+    `e^-rx*lx*bx`[i] <- exp(-R*x$`Number of leaves (age) x`[i]*lx[i]*x$`bx (seeds/stalk)`[i])}
+  
+  `sumofe^-rx*lybx`<- rev(cumsum(rev(`e^-rx*lx*bx`)))
+  
+  for (i in 1:length(x$Ax)){
+    vx[i]<- `e^(rx)/lx`[i]*`sumofe^-rx*lybx`[i+1]
+  }
+  
+  
+  x<- tibble(x$LatY, x$LongX, x$`Number of leaves (age) x`, x$Ax, x$`bx (seeds/stalk)`, 
+             Sx, lx, gx,`lx_bx`,`lx_bx_x`,
+             `lx*e^rx`, cx, `e^(rx)/lx`,
+             `e^-rx*lx*bx`, `sumofe^-rx*lybx`, vx)
+  
+  return(x)
 }
 
+#for every unique combination of coordinates
+#split data into seperate groups 
+x<- x %>% group_by(LatY, LongX) %>% group_split()
 
-#Reproductive value
-gx<- c()
-`lx_bx`<- c()
-`lx_bx_x`<- c()
+#apply the lifetable function to each group splitted by earlier
+bb<- lapply(x, Make_life_table)
 
-for (i in 1:length(x$Ax)) {
-  gx[i] <- lx[i+1]/lx[i]
-  
-  `lx_bx`[i]<- lx[i]*x$`bx (seeds/stalk)`[i]
-  
-  `lx_bx_x`[i]<- lx[i]*x$`bx (seeds/stalk)`[i]*x$`Number of leaves (age) x`[i]}
-
-Ro<- sum(`lx_bx`)
-G<- sum(`lx_bx_x`/Ro)
-R<- (log(Ro)/G)
-
-#Stable age distribution
-`lx*e^rx`<- c()
-cx<- c()
-`e^(rx)/lx`<- c()
-`e^-rx*lx*bx`<- c()
-`sumofe^-rx*lybx`<- c()
-vx<- c()
-
-
-for (i in 1:length(x$Ax)) {
-  `lx*e^rx`[i]<- (lx[i])*exp(-R*x$`Number of leaves (age) x`[i])
-  
-  cx[i] <- `lx*e^rx`[i]/sum(`lx*e^rx`)
-  
-  `e^(rx)/lx`[i] <- exp(-R*x$`Number of leaves (age) x`[i])/lx[i]
-  
-  `e^-rx*lx*bx`[i] <- exp(-R*x$`Number of leaves (age) x`[i]*lx[i]*x$`bx (seeds/stalk)`[i])}
-
-`sumofe^-rx*lybx`<- rev(cumsum(rev(`e^-rx*lx*bx`)))
-
-for (i in 1:length(x$Ax)){
-  vx[i]<- `e^(rx)/lx`[i]*`sumofe^-rx*lybx`[i+1]
-}
-
-
-x<- tibble(x$LatY, x$LongX, x$`Number of leaves (age) x`, x$Ax, x$`bx (seeds/stalk)`, 
-           Sx, lx, gx,`lx_bx`,`lx_bx_x`,
-           `lx*e^rx`, cx, `e^(rx)/lx`,
-           `e^-rx*lx*bx`, `sumofe^-rx*lybx`, vx)
-
-return(x)
+return(bb)
 }
 
 
